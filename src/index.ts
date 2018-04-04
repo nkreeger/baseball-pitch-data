f1a118dc81b527efc550520a34a11c004f9865d1
Nick Kreeger <nick.kreeger@gmail.com>
2018-04-04 09:09:50 -0700

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,22 @@
-import {BSON} from 'bson';
 import {writeFileSync} from 'fs';
-
 import {downloadDate} from './download';
-import {Pitch} from './pitchfx';
 
 async function downloadTest() {
-  let pitches = [] as Pitch[];
-
-  //   var now = new Date();
-  // var daysOfYear = [];
-  // for (var d = new Date(2012, 0, 1); d <= now; d.setDate(d.getDate() + 1)) {
-  //     daysOfYear.push(new Date(d));
-  // }
-
   const start = new Date('2017-7-1');
   const end = new Date('2017-7-31');
+
+  const outputs = [] as string[];
   for (const d = start; d <= end; d.setDate(d.getDate() + 1)) {
     console.log(d.toLocaleDateString());
-    pitches = pitches.concat(await downloadDate(d));
+    const pitches = await downloadDate(d);
+    for (let i = 0; i < pitches.length; i++) {
+      outputs.push(JSON.stringify(pitches[i]));
+    }
   }
 
-  console.log(`- Found ${pitches.length} pitches`);
-
-  // const bson = new BSON();
-  // const serialized = bson.serialize(pitches);
-  // console.log(serialized.length);
-
-  writeFileSync('test.bson', pitches);
-  console.log('---- done');
+  console.log(`- Found ${outputs.length} total pitches`);
+  writeFileSync('july_2017_pitches.json', outputs);
+  console.log('---- saved file.');
 }
 
 downloadTest();
